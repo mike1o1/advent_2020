ec5719c1e447195efe6a7ef1a6d5424bc7cfcc25
Mike Wilson <mike1234@gmail.com>
2020-11-30 21:48:09 -0800

    Remove index checks

--- day_1/solution.rb
+++ day_1/solution.rb
@@ -3,47 +3,43 @@ module Day1
     entries = entries(input)
     first = nil
     second = nil
-    
+
     # iterate through each entry and sum the others until
     # we get to 2020
-    entries.each_with_index do |entry, index|
-      entries.each_with_index do |second_entry, second_index|
-        if index != second_index
-          if entry + second_entry == 2020
-            first = entry
-            second = second_entry
-          end
+    entries.each do |entry|
+      entries.each do |second_entry|
+        if entry + second_entry == 2020
+          first = entry
+          second = second_entry
         end
       end
     end
-    
+
     first * second
   end
-  
+
   def self.problem_2(input)
     entries = entries(input)
-    
+
     first = nil
     second = nil
     third = nil
-    
-    entries.each_with_index do |entry, index|
-      entries.each_with_index do |second_entry, second_index|
-        entries.each_with_index do |third_entry, third_index|
-          if index != second_index && second_index != third_index
-            if entry + second_entry + third_entry == 2020
-              first = entry
-              second = second_entry
-              third = third_entry
-            end
+
+    entries.each do |entry|
+      entries.each_with_index do |second_entry|
+        entries.each_with_index do |third_entry|
+          if entry + second_entry + third_entry == 2020
+            first = entry
+            second = second_entry
+            third = third_entry
           end
         end
       end
     end
-    
+
     first * second * third
   end
-  
+
   def self.entries(input)
     input.split("\n").map(&:to_i)
   end
